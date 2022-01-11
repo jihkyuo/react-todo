60a08d90eb6ea02fd4c07a89c731ee7483dfc868
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2022-01-12 00:03:56 +0900

    category 수정

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,10 +1,11 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
 import { toDoState } from "./atoms";
+import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
-
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
@@ -12,7 +13,7 @@ function ToDoList() {
       <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
     </div>

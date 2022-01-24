10f59f4902017589777ad9a30555c79d2276c908
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2022-01-24 23:04:49 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&family=Viga&display=swap');
@@ -68,7 +67,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <ToDoList />
     </>
   );
 }
